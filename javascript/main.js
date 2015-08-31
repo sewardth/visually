fa526b7e6bb5f62fcbc97d23a2fa62916383cf3e
Tom Seward <sewardth@gmail.com>
2015-08-31 13:04:46 -0400

    Can now parse data!

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,7 @@
+var header = false;
+dropFile ={};
+fileData=[];
+
 $(document).ready(function() {
     console.log("loaded main.js");
 
@@ -16,7 +20,43 @@ $(document).ready(function() {
 	$("body").on("drop", function(event) {
 	    event.preventDefault();  
 	    event.stopPropagation();
+	    //fetch the file data
+	    dropFile = event.originalEvent.dataTransfer.files[0];
 	    $(this).removeClass('dragging');
+	    //display the loading modal
+	    $(".modal-title").html("Loading data from " + dropFile.name);
 	    $("#OpenModal").trigger("click");
 	});
 });
+
+function readCSV(file)
+{
+	  	Papa.parse(file, {
+		//worker: true,
+		header:header,
+		dynamicTyping:true,
+		step: function(row) {
+			fileData.push(row);
+		},
+		complete: function() {
+			console.log("Finished Reading File");
+			console.log(fileData);
+		}
+	});
+
+}
+
+
+function setHeaderValue(value)
+{
+	header = value;
+	console.log("header value set to " + header);
+	cleanModal();
+	readCSV(dropFile);
+}
+
+
+function cleanModal()
+{
+	$(".modal-body").html("Processing your data.  Please wait.");
+}
